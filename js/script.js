c6a6c75022a60cde2432843e826dda7b7f1fce16
GaiusCode <boluwatifegaius@gmail.com>
2022-10-05 20:31:19 +0100

    First push

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Technical Writer","Computer Hardware Technician"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
